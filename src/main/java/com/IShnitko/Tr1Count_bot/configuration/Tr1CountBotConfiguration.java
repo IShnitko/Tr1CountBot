297858ab3d77dcb0af9d49c0d6d91f4b981e5958
IShnitko <ilyshabogenika@gmail.com>
2025-08-01 19:12:05 +0200

    Specified profile

--- src/main/java/com/IShnitko/Tr1Count_bot/configuration/Tr1CountBotConfiguration.java
+++ src/main/java/com/IShnitko/Tr1Count_bot/configuration/Tr1CountBotConfiguration.java
@@ -4,6 +4,7 @@ import com.IShnitko.Tr1Count_bot.bot.Tr1CountBot;
 import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -12,6 +13,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class Tr1CountBotConfiguration {
 
     @Bean
+    @Profile("!test")
     public TelegramBotsApi telegramBotsApi(Tr1CountBot tr1CountBot) throws TelegramApiException {
         var api = new TelegramBotsApi(DefaultBotSession.class);
         api.registerBot(tr1CountBot);
