ab0cb75641b24847bb8df95bcce26835d59afeb1
IShnitko <ilyshabogenika@gmail.com>
2025-08-08 00:57:20 +0200

    Deleted unused methods

--- src/main/java/com/IShnitko/Tr1Count_bot/repository/ExpenseRepository.java
+++ src/main/java/com/IShnitko/Tr1Count_bot/repository/ExpenseRepository.java
@@ -1,6 +1,7 @@
 package com.IShnitko.Tr1Count_bot.repository;
 
 import com.IShnitko.Tr1Count_bot.model.Expense;
+import com.IShnitko.Tr1Count_bot.model.Group;
 import com.IShnitko.Tr1Count_bot.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,9 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-    List<Expense> findExpensesByPaidBy(User paidBy);
-
     @Query("select e from Expense e where e.paidBy = :paidBy and e.group.id = :groupId")
     List<Expense> findExpensesByPaidByFromGroup(User paidBy, Long groupId);
-
 }
