42074f3cf5af270c037887b4f92e86e4a4151606
IShnitko <ilyshabogenika@gmail.com>
2025-08-10 22:24:46 +0200

    Implemented custom exceptions for balance service

--- src/main/java/com/IShnitko/Tr1Count_bot/service/impl/BalanceServiceImpl.java
+++ src/main/java/com/IShnitko/Tr1Count_bot/service/impl/BalanceServiceImpl.java
@@ -10,6 +10,9 @@ import com.IShnitko.Tr1Count_bot.repository.ExpenseShareRepository;
 import com.IShnitko.Tr1Count_bot.repository.GroupRepository;
 import com.IShnitko.Tr1Count_bot.repository.UserRepository;
 import com.IShnitko.Tr1Count_bot.service.BalanceService;
+import com.IShnitko.Tr1Count_bot.util.exception.ExpenseNotFoundException;
+import com.IShnitko.Tr1Count_bot.util.exception.GroupNotFoundException;
+import com.IShnitko.Tr1Count_bot.util.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -68,13 +71,16 @@ public class BalanceServiceImpl implements BalanceService {
     public Map<User, BigDecimal> calculateBalance(String groupId) {
         Map<User, BigDecimal> balance = new HashMap<>();
         List<User> members = userRepository.findUsersByGroup(groupId);
+        if (members == null) throw new UserNotFoundException("Can't find members of the group " + groupId);
         for (User user : members) { // finding every user in the group
             List<Expense> eList = expenseRepository.findExpensesByPaidByFromGroup(user, groupId); // find every expense of that user in the certain group
+            if (eList == null) throw new ExpenseNotFoundException("Can't find expenses of the user " + user.getTelegramId() + " for group " + groupId);
             for (Expense e : eList) {
                 // here we take every expense of curr user, add full value of that expense to paidBy user
                 // then we take every expenseshare entity of that expense and subtract amount from each user associated with this expense
                 balance.put(user, balance.getOrDefault(user, BigDecimal.valueOf(0)).add(e.getAmount()));
                 List<ExpenseShare> esList = expenseShareRepository.findExpenseSharesByExpense(e); // get every es entity associated with this expense
+                if (esList == null) throw new ExpenseNotFoundException("Can't find expenseShared for expense " + e.getId());
                 for (ExpenseShare es : esList) {
                     balance.put(es.getUser(), balance.getOrDefault(es.getUser(), BigDecimal.valueOf(0)).subtract(es.getAmount()));
                 }
@@ -87,9 +93,8 @@ public class BalanceServiceImpl implements BalanceService {
     @Override
     public Expense updateExpense(Long expenseId, UpdateExpenseDto updateDto) {
         Expense expenseToUpdate = expenseRepository.findById(expenseId)
-                .orElseThrow(() -> new IllegalArgumentException("Expense wasn't found"));
+                .orElseThrow(() -> new ExpenseNotFoundException("Expense wasn't found"));
 
-        // update filed that are only in dto
         updateDto.paidByUserId().ifPresent(newPaidByUserId -> {
             User newPayer = userRepository.findUserByTelegramId(newPaidByUserId);
             expenseToUpdate.setPaidBy(newPayer);
@@ -126,6 +131,7 @@ public class BalanceServiceImpl implements BalanceService {
 
     @Override
     public List<Expense> getExpensesForGroup(String groupId) {
+        if (groupRepository.findGroupById(groupId) == null) throw new GroupNotFoundException("Can't find expenses for group " + groupId + ", because it doesn't exist");
         return expenseRepository.getExpensesByGroup(groupRepository.findGroupById(groupId));
     }
 }
