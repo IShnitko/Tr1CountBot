5686400e057472c5a54cee10b0f009832381b65b
IShnitko <ilyshabogenika@gmail.com>
2025-08-10 22:11:55 +0200

    Created custom exceptions

---
+++ src/main/java/com/IShnitko/Tr1Count_bot/util/exception/CreatorDeletionException.java
@@ -0,0 +1,7 @@
+package com.IShnitko.Tr1Count_bot.util.exception;
+
+public class CreatorDeletionException extends RuntimeException {
+    public CreatorDeletionException(String message) {
+        super(message);
+    }
+}
