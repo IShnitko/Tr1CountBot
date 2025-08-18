1c3b3798478e5557a345814514111d5ec66754c9
IShnitko <ilyshabogenika@gmail.com>
2025-08-19 01:50:51 +0200

    Created new error

---
+++ src/main/java/com/IShnitko/Tr1Count_bot/util/exception/NotEmptyHistoryException.java
@@ -0,0 +1,7 @@
+package com.IShnitko.Tr1Count_bot.util.exception;
+
+public class NotEmptyHistoryException extends RuntimeException {
+    public NotEmptyHistoryException(String message) {
+        super(message);
+    }
+}
