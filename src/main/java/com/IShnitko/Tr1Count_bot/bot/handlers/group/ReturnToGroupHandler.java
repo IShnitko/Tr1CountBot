278246b4f2bc04ea6d92bccba84afaf595ad509c
IShnitko <ilyshabogenika@gmail.com>
2025-08-19 01:51:41 +0200

    Created handler for general usage, so I don't have ot have a lot of unnecessary states with read only options

---
+++ src/main/java/com/IShnitko/Tr1Count_bot/bot/handlers/group/ReturnToGroupHandler.java
@@ -0,0 +1,34 @@
+package com.IShnitko.Tr1Count_bot.bot.handlers.group;
+
+import com.IShnitko.Tr1Count_bot.bot.context.ChatContext;
+import com.IShnitko.Tr1Count_bot.bot.handlers.StateHandler;
+import com.IShnitko.Tr1Count_bot.bot.handlers.annotation.StateHandlerFor;
+import com.IShnitko.Tr1Count_bot.bot.service.GroupManagementService;
+import com.IShnitko.Tr1Count_bot.bot.service.MessageService;
+import com.IShnitko.Tr1Count_bot.util.user_state.UserState;
+import com.IShnitko.Tr1Count_bot.util.user_state.UserStateManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import static com.IShnitko.Tr1Count_bot.bot.Tr1CountBot.BACK_COMMAND;
+
+@Component
+@StateHandlerFor(UserState.ONLY_RETURN_TO_GROUP)
+@RequiredArgsConstructor
+public class ReturnToGroupHandler implements StateHandler {
+    private final UserStateManager userStateManager;
+    private final GroupManagementService groupManagementService;
+    private final MessageService messageService;
+
+    @Override
+    public void handle(ChatContext context) throws Exception {
+        String command = context.getText() != null ? context.getText() : context.getCallbackData();
+        Long chatId = context.getChatId();
+        if (command.equals(BACK_COMMAND)) {
+            userStateManager.setState(chatId, UserState.IN_THE_GROUP);
+            groupManagementService.displayGroup(chatId, userStateManager.getChosenGroup(chatId), context.getMessage().getMessageId());
+        } else {
+            messageService.deleteMessage(chatId, context.getMessage().getMessageId());
+        }
+    }
+}
