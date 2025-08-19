48ea2c652952e18e3260404212a4f6a719a69386
IShnitko <ilyshabogenika@gmail.com>
2025-08-20 00:56:00 +0200

    Deleted obsolete exception

--- src/main/java/com/IShnitko/Tr1Count_bot/util/exception/InvalidDataException.java
+++
@@ -1,7 +0,0 @@
-package com.IShnitko.Tr1Count_bot.util.exception;
-
-public class InvalidDataException extends RuntimeException {
-    public InvalidDataException(String message) {
-        super(message);
-    }
-}
