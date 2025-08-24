0350cc4c65f5226c33485252485cf901e695e730
IShnitko <ilyshabogenika@gmail.com>
2025-08-24 21:43:12 +0200

    Deleted ru comments

--- src/main/java/com/IShnitko/Tr1Count_bot/util/GroupCodeGenerator.java
+++ src/main/java/com/IShnitko/Tr1Count_bot/util/GroupCodeGenerator.java
@@ -2,9 +2,6 @@ package com.IShnitko.Tr1Count_bot.util;
 
 import java.security.SecureRandom;
 
-/**
- * Утилитарный класс для генерации случайных кодов для групп.
- */
 public final class GroupCodeGenerator {
 
     private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -24,7 +21,6 @@ public final class GroupCodeGenerator {
 
         StringBuilder codeBuilder = new StringBuilder(length);
         for (int i = 0; i < length; i++) {
-            // Выбираем случайный символ из строки CHARACTERS
             int randomIndex = random.nextInt(CHARACTERS.length());
             codeBuilder.append(CHARACTERS.charAt(randomIndex));
         }
