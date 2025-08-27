158b54dd28c9e0866ce0051b5584b234d157d582
IShnitko <ilyshabogenika@gmail.com>
2025-08-27 22:21:46 +0200

    updated logic of group creation

--- src/main/java/com/IShnitko/Tr1Count_bot/bot/handlers/DefaultStateHandler.java
+++ src/main/java/com/IShnitko/Tr1Count_bot/bot/handlers/DefaultStateHandler.java
@@ -106,8 +106,10 @@ public class DefaultStateHandler implements StateHandler {
     }
 
     private void handleCreate(ChatContext context) {
-        messageService.editMessage(context.getChatId(), context.getMessage().getMessageId(), "Enter group name:", keyboardFactory.returnButton());
-        userStateManager.setState(context.getChatId(), UserState.AWAITING_GROUP_NAME);
+        Long chatId = context.getChatId();
+        messageService.editMessage(chatId, context.getMessage().getMessageId(), "Enter group name:", keyboardFactory.returnButton());
+        userStateManager.setState(chatId, UserState.AWAITING_GROUP_NAME);
+        userStateManager.setBotMessageId(chatId, context.getMessage().getMessageId());
     }
 
     private void chooseGroup(ChatContext context) {
