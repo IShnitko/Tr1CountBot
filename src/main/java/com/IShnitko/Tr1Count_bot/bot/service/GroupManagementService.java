396283d8b8135ee1e16c7d663a70ba380f319291
IShnitko <ilyshabogenika@gmail.com>
2025-08-27 22:22:23 +0200

    New formatting

--- src/main/java/com/IShnitko/Tr1Count_bot/bot/service/GroupManagementService.java
+++ src/main/java/com/IShnitko/Tr1Count_bot/bot/service/GroupManagementService.java
@@ -1,9 +1,21 @@
 package com.IShnitko.Tr1Count_bot.bot.service;
 
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
 public interface GroupManagementService {
     void groupHelpCommand(Long chatId, Integer messageId);
-    void displayGroup(Long chatId, String groupCode, Integer botMessageId, Integer inputMessageId);
+
     void displayGroup(Long chatId, String groupCode, Integer botMessageId);
 
+    void displayGroup(Long chatId, String groupCode, Integer botMessageId, Integer inputMessageId);
+
+    void displayGroup(Long chatId, String groupCode, Integer botMessageId, Integer inputMessageId, String additionalText);
+
     void sendIncorrectGroupId(Long chatId, Integer messageId) throws TelegramApiRequestException;
+
+    void sendJoinLink(Long chatId, Integer messageId);
+
+    void viewUserInfo(Long chatId, Integer messageId, Long userId);
+
+    void viewMembersMenu(Long chatId, Integer messageId, Long userId);
 }
