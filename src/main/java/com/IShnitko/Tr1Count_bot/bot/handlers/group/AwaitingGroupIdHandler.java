43d7b09f8208ed31f33dd58a7c5f6ff1b7f1a321
IShnitko <ilyshabogenika@gmail.com>
2025-09-03 01:40:47 +0200

    Changed logic to handle incorrect group id input

--- src/main/java/com/IShnitko/Tr1Count_bot/bot/handlers/group/AwaitingGroupIdHandler.java
+++ src/main/java/com/IShnitko/Tr1Count_bot/bot/handlers/group/AwaitingGroupIdHandler.java
@@ -5,11 +5,13 @@ import com.IShnitko.Tr1Count_bot.bot.handlers.state_handler.StateHandler;
 import com.IShnitko.Tr1Count_bot.bot.handlers.state_handler.annotation.StateHandlerFor;
 import com.IShnitko.Tr1Count_bot.bot.model.Command;
 import com.IShnitko.Tr1Count_bot.bot.service.GroupManagementService;
+import com.IShnitko.Tr1Count_bot.bot.service.MessageService;
 import com.IShnitko.Tr1Count_bot.bot.service.UserInteractionService;
 import com.IShnitko.Tr1Count_bot.service.GroupService;
 import com.IShnitko.Tr1Count_bot.bot.model.UserState;
 import com.IShnitko.Tr1Count_bot.bot.user_state.UserStateManager;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
@@ -17,35 +19,39 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 @Component
 @StateHandlerFor(UserState.AWAITING_GROUP_ID)
 @RequiredArgsConstructor
+@Slf4j
 public class AwaitingGroupIdHandler implements StateHandler {
     private final UserStateManager userStateManager;
     private final GroupManagementService groupManagementService;
     private final UserInteractionService userInteractionService;
     private final GroupService groupService;
+    private final MessageService messageService;
 
     @Override
     public void handle(ChatContext context) throws TelegramApiException {
         String input = context.getText() != null ? context.getText() : context.getCallbackData();
-        // Validate selection
+        Long chatId = context.getChatId();
+
         if (input.equals(Command.BACK_COMMAND.getCommand())) {
-            userStateManager.setState(context.getChatId(), UserState.DEFAULT);
-            userInteractionService.startCommand(context.getChatId(), context.getMessage().getMessageId());
+            userStateManager.setState(chatId, UserState.DEFAULT);
+            userInteractionService.startCommand(chatId, context.getMessage().getMessageId());
             return;
         }
-        handleValidSelection(context, input);
-    }
 
-    private void handleValidSelection(ChatContext context, String groupId) throws TelegramApiRequestException {
-        Long chatId = context.getChatId();
-        if (groupService.doesGroupExist(groupId)) {
-            userStateManager.setStateWithChosenGroup(chatId, UserState.IN_THE_GROUP, groupId);
-            if (!groupService.doesUserExistInGroup(context.getUser().getId(), groupId)) groupService.joinGroupById(groupId, context.getUser().getId());
-            groupManagementService.displayGroup(chatId,
-                    groupId,
-                    userStateManager.getBotMessageId(chatId),
-                    context.getUpdateType() == ChatContext.UpdateType.MESSAGE ? context.getMessage().getMessageId() : null); // if user inputted groupId manually, then we delete his message
+        if (groupService.doesGroupExist(input)) {
+            log.info("Group was found with ID: {}", input);
+            userStateManager.setStateWithChosenGroup(chatId, UserState.IN_THE_GROUP, input);
+
+            if (!groupService.doesUserExistInGroup(context.getUser().getId(), input)) {
+                groupService.joinGroupById(input, context.getUser().getId());
+            }
+
+            Integer botMessageId = userStateManager.getBotMessageId(chatId);
+            Integer inputMessageId = context.getUpdateType() == ChatContext.UpdateType.MESSAGE ? context.getMessage().getMessageId() : null;
+
+            groupManagementService.displayGroup(chatId, input, botMessageId, inputMessageId);
         } else {
-            groupManagementService.sendIncorrectGroupId(chatId, context.getMessage().getMessageId());
+            groupManagementService.sendIncorrectGroupId(chatId, context.getMessage().getMessageId(), context.getUser().getId()); // throws error message is not modified: specified new message content and reply markup are exactly the same as a current content and reply markup of the message
         }
     }
 }
