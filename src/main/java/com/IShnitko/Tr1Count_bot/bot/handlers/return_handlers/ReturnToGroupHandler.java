480afba2635801152a4b9bb97427b1261eab278d
IShnitko <ilyshabogenika@gmail.com>
2025-09-03 01:37:35 +0200

    Created new package for handling returns

--- src/main/java/com/IShnitko/Tr1Count_bot/bot/handlers/return_handlers/ReturnToGroupHandler.java
+++ src/main/java/com/IShnitko/Tr1Count_bot/bot/handlers/return_handlers/ReturnToGroupHandler.java
@@ -1,4 +1,4 @@
-package com.IShnitko.Tr1Count_bot.bot.handlers.group;
+package com.IShnitko.Tr1Count_bot.bot.handlers.return_handlers;
 
 import com.IShnitko.Tr1Count_bot.bot.context.ChatContext;
 import com.IShnitko.Tr1Count_bot.bot.handlers.state_handler.StateHandler;
@@ -6,16 +6,15 @@ import com.IShnitko.Tr1Count_bot.bot.handlers.state_handler.annotation.StateHand
 import com.IShnitko.Tr1Count_bot.bot.model.Command;
 import com.IShnitko.Tr1Count_bot.bot.service.GroupManagementService;
 import com.IShnitko.Tr1Count_bot.bot.service.MessageService;
-import com.IShnitko.Tr1Count_bot.bot.user_state.UserStateManager;
 import com.IShnitko.Tr1Count_bot.bot.model.UserState;
+import com.IShnitko.Tr1Count_bot.bot.user_state.UserStateManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-
 @Component
-@StateHandlerFor(UserState.ONLY_RETURN_TO_MEMBERS_MENU)
+@StateHandlerFor(UserState.ONLY_RETURN_TO_GROUP)
 @RequiredArgsConstructor
-public class ReturnToMembersMenuHandler implements StateHandler {
+public class ReturnToGroupHandler implements StateHandler {
     private final UserStateManager userStateManager;
     private final GroupManagementService groupManagementService;
     private final MessageService messageService;
@@ -24,12 +23,11 @@ public class ReturnToMembersMenuHandler implements StateHandler {
     public void handle(ChatContext context) throws Exception {
         String command = context.getText() != null ? context.getText() : context.getCallbackData();
         Long chatId = context.getChatId();
-        Integer messageId = context.getMessage().getMessageId();
         if (command.equals(Command.BACK_COMMAND.getCommand())) {
-            userStateManager.setState(chatId, UserState.MEMBERS_MENU);
-            groupManagementService.viewMembersMenu(chatId, messageId, context.getUser().getId());
+            userStateManager.setState(chatId, UserState.IN_THE_GROUP);
+            groupManagementService.displayGroup(chatId, userStateManager.getChosenGroup(chatId), context.getMessage().getMessageId());
         } else {
-            messageService.deleteMessage(chatId, messageId);
+            messageService.deleteMessage(chatId, context.getMessage().getMessageId());
         }
     }
 }
