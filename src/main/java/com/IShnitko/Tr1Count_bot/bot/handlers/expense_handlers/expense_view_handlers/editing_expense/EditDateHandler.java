7c793d325cd4f9cac0f2c3da0b09e9b99aa90f91
IShnitko <ilyshabogenika@gmail.com>
2025-09-25 15:14:50 +0200

    Added implementation of EditDateHandler

--- src/main/java/com/IShnitko/Tr1Count_bot/bot/handlers/expense_handlers/expense_view_handlers/editing_expense/EditDateHandler.java
+++ src/main/java/com/IShnitko/Tr1Count_bot/bot/handlers/expense_handlers/expense_view_handlers/editing_expense/EditDateHandler.java
@@ -9,12 +9,15 @@ import com.IShnitko.Tr1Count_bot.bot.model.UserState;
 import com.IShnitko.Tr1Count_bot.bot.service.impl.ExpenseManagementServiceImpl;
 import com.IShnitko.Tr1Count_bot.bot.service.impl.MessageServiceImpl;
 import com.IShnitko.Tr1Count_bot.bot.user_state.UserStateManager;
+import com.IShnitko.Tr1Count_bot.dto.ExpenseUpdateDto;
 import com.IShnitko.Tr1Count_bot.service.impl.BalanceServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 @Slf4j
 @Component
@@ -38,6 +41,7 @@ public class EditDateHandler implements StateHandler {
             expenseManagementService.sendInvalidDateInput(chatId, messageId);
             return;
         }
+
         if (input.equalsIgnoreCase(Command.BACK_COMMAND.getCommand())) {
             userStateManager.setState(chatId, UserState.EXPENSE_INFO);
             messageService.editMessage(context.getChatId(), context.getMessage().getMessageId(),
@@ -48,5 +52,33 @@ public class EditDateHandler implements StateHandler {
             return;
         }
 
+        if (input.equalsIgnoreCase(Command.DEFAULT_DATE_COMMAND.getCommand())) {
+            userStateManager.setState(chatId, UserState.EXPENSE_INFO);
+            ExpenseUpdateDto expenseUpdateDto = userStateManager.getOrCreateExpenseUpdateDto(chatId);
+            expenseUpdateDto.setDate(LocalDate.now().atStartOfDay());
+            messageService.editMessage(context.getChatId(), context.getMessage().getMessageId(),
+                    balanceService.getExpenseTextFromExpenseDTO(
+                            expenseUpdateDto
+                    ),
+                    keyboardFactory.expenseDetailsKeyboard());
+            return;
+        }
+
+        try {
+            LocalDate expenseDate = LocalDate.parse(input, DATE_FORMATTER);
+            userStateManager.setState(chatId, UserState.EXPENSE_INFO);
+            ExpenseUpdateDto expenseUpdateDto = userStateManager.getOrCreateExpenseUpdateDto(chatId);
+            expenseUpdateDto.setDate(expenseDate.atStartOfDay());
+
+            messageService.deleteMessage(chatId, context.getMessage().getMessageId()); // TODO: extract to service
+            messageService.editMessage(context.getChatId(), userStateManager.getBotMessageId(chatId),
+                    balanceService.getExpenseTextFromExpenseDTO(
+                            expenseUpdateDto
+                    ),
+                    keyboardFactory.expenseDetailsKeyboard());
+
+        } catch (DateTimeParseException e) {
+            expenseManagementService.sendInvalidDateInput(chatId, messageId);
+        }
     }
 }
